35e6794f7083a30cda6f1cbcfc2c9aa5a1ab5bc2
summydev <adegbitesumayah@gmail.com>
2024-12-08 22:38:37 +0100

    minor change

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Text('hello world'),
+      home: const Text(' '),
     );
   }
 }
