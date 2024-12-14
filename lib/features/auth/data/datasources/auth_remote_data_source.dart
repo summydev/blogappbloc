60fa8e229a5ff8df3bb107c0a8095fc0d7ccbe1b
summydev <adegbitesumayah@gmail.com>
2024-12-14 16:20:43 +0100

    done with the presentation layer for now

--- lib/features/auth/data/datasources/auth_remote_data_source.dart
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -4,4 +4,24 @@ abstract interface class AuthRemoteDataSource {
     required String email,
     required String password,
   });
+  Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  @override
+  Future<String> loginWithEmailPassword(
+      {required String email, required String password}) {
+    // TODO: implement loginWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signUpWithEmailPassword(
+      {required String name, required String email, required String password}) {
+    // TODO: implement signUpWithEmailPassword
+    throw UnimplementedError();
+  }
 }
